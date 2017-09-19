4fae560da521846554eab4b30428e2cbcaa12772
moshanpi <spencer_mo@mcafee.com>
2017-09-19 15:52:22 +0800

    fetch remote data

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,86 +1,105 @@
 import React from 'react';
-import {Text, View, Image, StyleSheet, Button,TextInput} from 'react-native';
+import {Text, View, Image, StyleSheet, Button, TextInput} from 'react-native';
 import {NativeEventEmitter, NativeModules} from 'react-native';
 import {connect} from 'react-redux';
-import {increase,decrease,reset} from './actions/actions';
+import {increase, decrease, reset} from './actions/actions';
 
 class HomeScreen extends React.Component {
- a = 11;
+  a = 11;
 
-static b = 2;
-  constructor(props){
+  static b = 2;
+
+  constructor(props) {
     super(props);
     this.c = 13;
-    this.state = {showText:true,
-      dis:'haha',
+    this.state = {
+      showText: true,
+      dis: 'haha',
     };
 
     // console.log('this.a '+this.a);
 
 
-    setInterval(()=>{
-      this.setState(previousState=>{
+    setInterval(() => {
+      this.setState(previousState => {
         // console.log("this.name "+this.name);
         // console.log("this.addr "+this.addr);
-        return {showText:!this.state.showText};
+        return {showText: !this.state.showText};
       });
-    },1000);
+    }, 1000);
   }
 
 
-componentDidMout(){
+  componentDidMout() {
     alert('componentDidMount');
     // console.log('did mount');
     this.dd = 1;
 
-}
+  }
 
 // getDefaultProps(){
 //   alert('getDefaultProps');
 // }
 
-componentWillUnmount() {
+  componentWillUnmount() {
     // alert('componentWillUnmount');
     // console.log('componentWillUnmount');
 
   }
 
 
-componentWillUpdate() {
-  // alert(this.dd);
-  // console.log('componentWillUpdate');
-  this.name = 'msp22';
-  this.addr = 'huoxing';
-}
+  componentWillUpdate() {
+    // alert(this.dd);
+    // console.log('componentWillUpdate');
+    this.name = 'msp22';
+    this.addr = 'huoxing';
+  }
 
-textChange=(text)=>{
-  // console.log(text);
-};
+  textChange = (text) => {
+    // console.log(text);
+  };
 
-onSubmit=()=>{
-  // console.log('onSubmit');
-}
+  onSubmit = () => {
+    // console.log('onSubmit');
+  }
 
-increase = () => {
+  increase = () => {
     console.log('increase');
     this.props.dispatch(increase());
-}
+  }
 
-decrease = () => {
+  decrease = () => {
     console.log('decrease');
     this.props.dispatch(decrease());
-}
+  }
 
-reset = () => {
+
+  reset = () => {
     console.log('reset');
     // this.props.dispatch(reset());
-    
-    this.props.dispatch(dispatch=>{
-      setTimeout(()=>{
+
+    this.props.dispatch(dispatch => {
+      setTimeout(() => {
         this.props.dispatch(reset());
-      },2000);
+      }, 2000);
     });
-}
+  }
+
+  getDataFromServer = () => {
+    console.log('getDataFromServer');
+
+    fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log('getData: '+responseJson.movies);
+      })
+      .catch((error)=>{
+        console.log('error');
+        console.error(error);
+      });
+
+  }
+
 
   render() {
 
@@ -91,9 +110,9 @@ reset = () => {
     const eventEmitter = new NativeEventEmitter(MyEventEmitter);
 
     // alert(eventEmitter);
-                      //    eventEmitter.addListener('EventReminder',(reminder)=>
-                      //    console.log(reminder.name)
-                      //  );
+    //    eventEmitter.addListener('EventReminder',(reminder)=>
+    //    console.log(reminder.name)
+    //  );
     // const subscription = eventEmitter.addListener('EventReminder',(reminder)=> alert(reminder.name));
 
     // 别忘了取消订阅，通常在componentWillUnmount生命周期方法中实现。
@@ -102,12 +121,12 @@ reset = () => {
     const {navigate} = this.props.navigation;
     var date = new Date();
     var contacts = NativeModules.ContactsService;
-    contacts.addEvent('aa','bb');
+    contacts.addEvent('aa', 'bb');
     contacts.addEvent2('Birthday Party', '4 Privet Drive, Surrey', date.getTime());
     contacts.sayHello('xiyun');
-    contacts.sayHello2('zhangsan','how','are','you');
-    contacts.findEvents((error,content) => {
-      if(error) {
+    contacts.sayHello2('zhangsan', 'how', 'are', 'you');
+    contacts.findEvents((error, content) => {
+      if (error) {
 
       } else {
         // console.log(content);
@@ -115,41 +134,41 @@ reset = () => {
     });
 
 
-
     return (
       <View>
+        <Button title="获取远程数据" onPress={this.getDataFromServer}/>
         <TextInput style={styles.input}
-          placeholder="哈哈哈"
-          onChangeText={this.textChange}
-          onSubmitEditing={this.onSubmit}
-          ></TextInput>
+                   placeholder="哈哈哈"
+                   onChangeText={this.textChange}
+                   onSubmitEditing={this.onSubmit}
+        ></TextInput>
         <Button onPress={() => navigate('Chat', {user: 'XiYun'})} title="Chat with XiYun"/>
 
         {/* <Image source={{uri:'girl.png'}} style={{width:400,height:400}}></Image> */}
         <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-       style={{width: 400, height: 400}} />
-       <Text>使用redux管理状态的计数器</Text>
-       <Text  style={{backgroundColor:'red'}}>{this.props.counter2.count}</Text>
-       <Button title="+1" onPress={this.increase}/>
-       <Button title="-1" onPress={this.decrease}/>
-       <Button title="归0" onPress={this.reset}/>
+               style={{width: 400, height: 400}}/>
+        <Text>使用redux管理状态的计数器</Text>
+        <Text style={{backgroundColor: 'red'}}>{this.props.counter2.count}</Text>
+        <Button title="+1" onPress={this.increase}/>
+        <Button title="-1" onPress={this.decrease}/>
+        <Button title="归0" onPress={this.reset}/>
       </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
-  input:{
-    width:200,
-    height:50,
-    backgroundColor:'#00ff00',
+  input: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#00ff00',
   }
 });
 
 //一定要进行redux和componet的绑定，否则this.props.dispatch等相关方法无法调用。
 
 const mapStateToProps = state => ({
-  counter2:state.counter //key 将会绑定到this.props中,值是在reducer中定义的回调函数 。
+  counter2: state.counter //key 将会绑定到this.props中,值是在reducer中定义的回调函数 。
 })
 
 export default connect(mapStateToProps)(HomeScreen);
