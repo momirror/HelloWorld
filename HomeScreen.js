975970f34cd0b6f6e1f053033f73d795493ee10a
moshanpi <spencer_mo@mcafee.com>
2017-09-29 08:38:49 +0800

    test

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View, Image, StyleSheet, Button, TextInput} from 'react-native';
-import {NativeEventEmitter, NativeModules} from 'react-native';
+import {NativeEventEmitter, NativeModules, Animated} from 'react-native';
 import {connect} from 'react-redux';
 import {increase, decrease, reset} from './actions/actions';
 
@@ -15,7 +15,9 @@ class HomeScreen extends React.Component {
     this.state = {
       showText: true,
       dis: 'haha',
+      fadeAnim: new Animated.Value(0),
     };
+    this._textInput = null;
 
     // console.log('this.a '+this.a);
 
@@ -35,6 +37,7 @@ class HomeScreen extends React.Component {
     // console.log('did mount');
     this.dd = 1;
 
+
   }
 
 // getDefaultProps(){
@@ -53,6 +56,13 @@ class HomeScreen extends React.Component {
     // console.log('componentWillUpdate');
     this.name = 'msp22';
     this.addr = 'huoxing';
+
+    Animated.timing(
+      this.state.fadeAnim, {
+        toValue: 1,
+//        duration:2000,
+      }
+    ).start();
   }
 
   textChange = (text) => {
@@ -88,16 +98,50 @@ class HomeScreen extends React.Component {
   getDataFromServer = () => {
     console.log('getDataFromServer');
 
-    fetch('https://facebook.github.io/react-native/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log('getData: '+responseJson.movies);
-      })
-      .catch((error)=>{
-        console.log('error');
-        console.error(error);
-      });
+    var url;
 
+//    url = 'https://facebook.github.io/react-native/movies.json';
+    //    fetch(url)
+//      .then((response) => response.json())
+//      .then((responseJson) => {
+//        console.log('getData: ' + responseJson.movies);
+//      })
+//      .catch((error) => {
+//        console.log('error');
+//        console.error(error);
+//      });
+
+//    url = 'http://192.168.1.104:3000/secrets';
+//    fetch(url,'GET')
+//      .then(
+//        (response) => {
+//          console.log('getData: ' + response)
+//        });
+
+    fetch('http://192.168.1.104:3000/secrets', {
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
+      return response.json();
+//      return response.text();
+      //获取数据,数据处理
+    }).then(function (data) {
+      console.log(data);
+    }).
+    catch(function(err) {
+      //错误处理
+      console.log(err);
+    });
+
+
+  }
+
+  updateNativeProps = (value) => {
+    console.log("updateNativeProps");
+    this._textInput.setNativeProps({
+      opacity: 0.4,
+      backgroundColor: 'yellow'
+    });
   }
 
 
@@ -106,17 +150,21 @@ class HomeScreen extends React.Component {
     // console.log(this.name);
     // console.warn('aaaa');
 
-    const {MyEventEmitter} = NativeModules;
+    const {
+      MyEventEmitter
+    }
+
+      = NativeModules;
     const eventEmitter = new NativeEventEmitter(MyEventEmitter);
 
-    // alert(eventEmitter);
-    //    eventEmitter.addListener('EventReminder',(reminder)=>
-    //    console.log(reminder.name)
-    //  );
-    // const subscription = eventEmitter.addListener('EventReminder',(reminder)=> alert(reminder.name));
+// alert(eventEmitter);
+//    eventEmitter.addListener('EventReminder',(reminder)=>
+//    console.log(reminder.name)
+//  );
+// const subscription = eventEmitter.addListener('EventReminder',(reminder)=> alert(reminder.name));
 
-    // 别忘了取消订阅，通常在componentWillUnmount生命周期方法中实现。
-    // subscription.remove();
+// 别忘了取消订阅，通常在componentWillUnmount生命周期方法中实现。
+// subscription.remove();
 
     const {navigate} = this.props.navigation;
     var date = new Date();
@@ -137,7 +185,8 @@ class HomeScreen extends React.Component {
     return (
       <View>
         <Button title="获取远程数据" onPress={this.getDataFromServer}/>
-        <TextInput style={styles.input}
+        <Button title="SetNatieProps" onPress={this.updateNativeProps}/>
+        <TextInput ref={(c) => this._textInput = c} style={styles.input}
                    placeholder="哈哈哈"
                    onChangeText={this.textChange}
                    onSubmitEditing={this.onSubmit}
@@ -147,7 +196,7 @@ class HomeScreen extends React.Component {
         {/* <Image source={{uri:'girl.png'}} style={{width:400,height:400}}></Image> */}
         <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
                style={{width: 400, height: 400}}/>
-        <Text>使用redux管理状态的计数器</Text>
+        <Animated.Text style={{opacity: this.state.fadeAnim}}>使用redux管理状态的计数器</Animated.Text>
         <Text style={{backgroundColor: 'red'}}>{this.props.counter2.count}</Text>
         <Button title="+1" onPress={this.increase}/>
         <Button title="-1" onPress={this.decrease}/>
